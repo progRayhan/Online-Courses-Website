15def004d8bb9e54f831fa8c21cf5a0348463910
Rayhan <prog.rayhan1@gmail.com>
2022-10-31 16:07:34 +0600

    inline form added

--- programming_buddies/courses/admin.py
+++ programming_buddies/courses/admin.py
@@ -2,7 +2,16 @@ from django.contrib import admin
 from courses.models import Course, Tag, Prerequisite, Learning
 
 # Register your models here.
-admin.site.register(Course)
-admin.site.register(Tag)
-admin.site.register(Prerequisite)
-admin.site.register(Learning)
+class TagAdmin(admin.TabularInline):
+    model = Tag
+
+class PrerequisiteAdmin(admin.TabularInline):
+    model = Prerequisite
+
+class LearningAdmin(admin.TabularInline):
+    model = Learning
+
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [TagAdmin, PrerequisiteAdmin, LearningAdmin]
+
+admin.site.register(Course, CourseAdmin)
