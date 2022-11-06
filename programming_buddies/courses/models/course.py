0d256aaf430fd2872f456426cf181f6db68a6e33
Rayhan <prog.rayhan1@gmail.com>
2022-11-06 16:29:01 +0600

    Adding Course & Videos in admin panel

--- programming_buddies/courses/models/course.py
+++ programming_buddies/courses/models/course.py
@@ -26,7 +26,7 @@ class Course(models.Model):
 
 
 class CourseProperty(models.Model):
-    description = models.CharField(max_length=20, null=False)
+    description = models.CharField(max_length=100, null=False)
     course = models.ForeignKey(Course, null=False, on_delete=models.CASCADE)
 
     class Meta:
